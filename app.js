102947313944aae9e8a60904044e5779212ab694
AE 86 <kipupuseja@users.noreply.github.com>
2017-09-19 18:14:16 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var port = process.argv[2] || 8080;
 var ip = process.argv[3] || '127.0.0.1';
 var app = express();
+var request = require('request');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
@@ -11,6 +12,21 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res, next) {
   res.render('index', { title: 'A test app', node_version: process.version });
+});
+
+app.get('/http/daocloud/restart/:appid/:token',function(req, res){
+    var appid = req.params.appid;
+    var token = req.params.token;
+    request({
+        method: 'POST',
+        url:"https://openapi.daocloud.io/v1/apps/"+ appid +"/actions/restart",
+        headers: {"Authorization": token}}, 
+        function (error, response, body) {
+            if(error)
+                res.send(error);
+            else
+                res.send(body);
+    });
 });
 
 app.listen(port, ip, function () {
